fd9a67aa7196dda82c310b937539a4b0ab954dae
Documenter.jl <documenter@juliadocs.github.io>
2025-02-11 09:14:00 +0000

    build based on c091e52

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = B0Map","category":"page"},{"location":"#B0Map","page":"Home","title":"B0Map","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for B0Map.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [B0Map]","category":"page"}]
+}
